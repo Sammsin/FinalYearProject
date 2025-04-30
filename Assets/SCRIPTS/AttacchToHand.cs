48e2aeb9313c7a0a86af44b2dcbcc6e1a62616ff
Sammsin <sampedroschaerer@gmail.com>
2025-04-30 18:14:18 +0100

    Added UI interactability

--- Assets/SCRIPTS/AttacchToHand.cs
+++ Assets/SCRIPTS/AttacchToHand.cs
@@ -1,21 +1,33 @@
 using UnityEngine;
 using UnityEngine.XR;
-using UnityEngine.XR.Hands; // Requires Unity's XR Hands package
 
 public class AttachToPalm : MonoBehaviour
 {
     public XRNode handNode = XRNode.RightHand;
+    public Vector3 positionOffset = new Vector3(0, 0, 0.1f);
+    public Vector3 rotationOffset = Vector3.zero;
+
+    private InputDevice device;
+    private bool deviceInitialized = false;
 
     void Update()
     {
-        InputDevice device = InputDevices.GetDeviceAtXRNode(handNode);
-
-        if (device.TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 pos) &&
-            device.TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rot))
+        if (!deviceInitialized || !device.isValid)
         {
-            transform.position = pos;
-            transform.rotation = rot;
+            device = InputDevices.GetDeviceAtXRNode(handNode);
+            deviceInitialized = device.isValid;
+        }
+
+        if (deviceInitialized)
+        {
+            if (device.TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position) &&
+                device.TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation))
+            {
+                // Apply rotation offset
+                Quaternion adjustedRotation = rotation * Quaternion.Euler(rotationOffset);
+                transform.position = position + adjustedRotation * positionOffset;
+                transform.rotation = adjustedRotation;
+            }
         }
     }
 }
-
